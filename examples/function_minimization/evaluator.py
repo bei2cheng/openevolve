6c0b6dec7b4f0dbd5adc618223bcf0c97083ce6a
fengzhimeng <fengzhimeng@jiuaoedu.com>
2026-05-09 15:48:53 +0800

    .

--- examples/function_minimization/evaluator.py
+++ examples/function_minimization/evaluator.py
@@ -58,9 +58,9 @@ def evaluate(program_path):
     # GLOBAL_MIN_Y = 0.678
     # GLOBAL_MIN_VALUE = -1.519
 
-    GLOBAL_MIN_X = -1.304
-    GLOBAL_MIN_Y = 0.278
-    GLOBAL_MIN_VALUE = -1.1944
+    GLOBAL_MIN_X = 2.504
+    GLOBAL_MIN_Y = 3.378
+    GLOBAL_MIN_VALUE = 1.1365
 
     try:
         # Load the program
@@ -269,9 +269,9 @@ def evaluate_stage1(program_path):
     # GLOBAL_MIN_Y = float(0.678)
     # GLOBAL_MIN_VALUE = float(-1.519)
 
-    GLOBAL_MIN_X = float(-1.304)
-    GLOBAL_MIN_Y = float(0.278)
-    GLOBAL_MIN_VALUE = float(-1.1944)
+    GLOBAL_MIN_X = float(2.504)
+    GLOBAL_MIN_Y = float(3.378)
+    GLOBAL_MIN_VALUE = float(1.1365)
 
     # Quick check to see if the program runs without errors
     try:
